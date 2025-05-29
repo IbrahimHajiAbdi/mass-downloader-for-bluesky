f5b9cb2af955e8f2f2d26f34522235edb6c8907e
Ibrahim Haji Abdi <ibrahim.hajiabdi09@gmail.com>
2025-05-29 22:29:59 +0100

    fixed error with restore not working if no username/did was specified, instead of restoring the ehole database

--- mdfb/mdfb.py
+++ mdfb/mdfb.py
@@ -9,11 +9,11 @@ from mdfb.core.resolve_handle import resolve_handle
 from mdfb.utils.validation import validate_database, validate_directory, validate_download, validate_format, validate_limit, validate_no_posts, validate_threads
 from mdfb.utils.helpers import split_list
 from mdfb.utils.cli_helpers import account_or_did, get_did
-from mdfb.utils.database import connect_db, delete_user, check_user_has_posts
+from mdfb.utils.database import connect_db, delete_user, check_user_has_posts, restore_posts
 from mdfb.utils.logging import setup_logging, setup_resource_monitoring
 from mdfb.utils.constants import DEFAULT_THREADS, MAX_THREADS 
 
-def fetch_posts(did: str, post_types: dict, limit: int = 0, archive: bool = False, update: bool = False, media_types: list[str] = None, num_threads: int = 1, restore: bool = False) -> list[dict]:
+def fetch_posts(did: str, post_types: dict[str, bool], limit: int = 0, archive: bool = False, update: bool = False, media_types: list[str] = None, num_threads: int = 1, restore: bool = False) -> list[dict]:
     post_uris = []
     for post_type, wanted in post_types.items():
         if wanted:
@@ -25,6 +25,8 @@ def fetch_posts(did: str, post_types: dict, limit: int = 0, archive: bool = Fals
             else:
                 if media_types:
                     post_uris.extend(get_post_identifiers_media_types(did, post_type, media_types, limit=limit, archive=archive, update=update, num_threads=num_threads, restore=restore))
+                elif restore:
+                    post_uris.extend(restore_posts(did, {post_type: wanted}))
                 else:
                     post_uris.extend(get_post_identifiers(did, post_type, limit=limit, archive=archive, update=update))
     return post_uris
